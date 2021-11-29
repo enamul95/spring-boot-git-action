d92b928ac5072d986c3e7efe34327a23348d1f1f
enamul95 <ehaque95@gmail.com>
2021-11-30 00:12:52 +0600

    first code

---
+++ src/main/java/com/app/springbootgitaction/SpringBootGitActionApplication.java
@@ -0,0 +1,20 @@
+package com.app.springbootgitaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootGitActionApplication {
+	
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome to javatechie !";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitActionApplication.class, args);
+	}
+
+}
